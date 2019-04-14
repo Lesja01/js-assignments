c70bff81f7a2e2e097edb1a1f1827ebbb60aacb6
Lesja01 <Lesja01@mail.ru>
2019-04-14 18:19:13 +0300

    add solution getPermutation

--- task/12-katas-3-tasks.js
+++ task/12-katas-3-tasks.js
@@ -44,8 +44,26 @@ function findStringInSnakingPuzzle(puzzle, searchStr) {
  *    'ab'  => 'ab','ba'
  *    'abc' => 'abc','acb','bac','bca','cab','cba'
  */
-function* getPermutations(chars) {
-    throw new Error('Not implemented');
+function getPermutations(string) {
+    var results = [];
+
+      if (string.length === 1) 
+      {
+        results.push(string);
+        return results;
+      }
+
+      for (var i = 0; i < string.length; i++) 
+      {
+        var firstChar = string[i];
+        var otherChar = string.substring(0, i) + string.substring(i + 1);
+        var otherPermutations = getPermutations(otherChar);
+        
+        for (var j = 0; j < otherPermutations.length; j++) {
+          results.push(firstChar + otherPermutations[j]);
+        }
+      }
+      return results;
 }
 
 
